119e720da2499dfd8776edeaddebb6fed4f0a56f
Adam Michel <awmichel90@gmail.com>
2012-04-19 19:51:38 -0400

    Remove autosubscribe and manually publish collections.

--- server/publish.js
+++ server/publish.js
@@ -1,1 +1,18 @@
+// Suggestions -- {content: String, 
+//                 success: Boolean}
 Suggestions = new Meteor.Collection("suggestions");
+
+// Publish list of all suggestions to clients.
+Meteor.publish('suggestions', function() {
+  return Suggestions.find();
+});
+
+// Comments -- {content: String,
+//              suggestion_id: String,
+//              timestamp: Number}
+Comments = new Meteor.Collection("comments");
+
+// Publish comments for the requested suggestion.
+Meteor.publish('comments', function(suggestion_id) {
+  return Comments.find({suggestion_id: suggestion_id});
+});
